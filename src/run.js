b9e9a404e3d717e35eb29e75354887612634d3b5
Ben Junya <bjunya@gmail.com>
2018-08-04 23:15:46 -0700

    Made renderFile() actually render files

--- src/run.js
+++ src/run.js
@@ -4,12 +4,14 @@ import Chalk from 'chalk';
 import Inquirer from 'inquirer';
 import Ejs from 'ejs';
 import path from 'path';
+
 import fs from 'fs'; // @TODO: Remove this module and use fse instead
 import fse from 'fs-extra';
 
 type SproutConfigDescriptor = {
   templateDir: string
 };
+
 /** Sprout Root configuration file */
 type SproutRootConfig = () => SproutConfigDescriptor;
 
@@ -109,22 +111,63 @@ async function getTemplateQuestions(template: string) {
   };
 }
 
-async function renderFile(outputDirectory, filename, variables) {
+/**
+ * Renders a single file
+ * @param  {String} options.templatePath - Absolute path to the EJS Template
+ * @param  {String} options.filename     - Absolute path to the file to create
+ * @param  {String} options.variables    - Variables to render via EJS
+ * @return {undefined}
+ */
+async function renderFile({ templatePath, filename , variables }): Promise<void> {
   try {
-
+    console.log(Chalk.yellow`WRITE: `, filename);
+    Ejs.renderFile(templatePath, variables, (error, file) => {
+      fse.writeFileSync(filename, file);
+    });
   } catch (error) {
-    throw err;
+    console.error(error);
+    throw error;
   }
 }
 
-async function renderDirectory(outputDirectory, filename, variables) {
+/**
+ * Creates a directory, and then renders all files in a template directory.
+ * @param  {String} options.templatePath - Absolute path to the EJS template
+ * @param  {String} options.directory    - Absolute path to the new directory to be created
+ * @param  {String} options.variables    - Variables to render with
+ * @return {Promise<undefined>}
+ */
+async function renderDirectory({ templatePath, directory, variables }): Promise<void> {
   try {
 
-    console.log('MKDIR', outputDirectory);
-    fs.mkdirSync(path.resolve())
-  } catch (error) {
+    console.log(Chalk.yellow`MKDIR`, directory);
+    fse.mkdirSync(directory);
 
-    throw err;
+    const dir = fse.readdirSync(templatePath);
+    dir.forEach( async file => {
+      const fileStats = fse.statSync(path.resolve(templatePath, file));
+
+      if (fileStats.isDirectory()) {
+
+        //@TODO: Replace by filename logic here
+        await renderDirectory({
+          templatePath: path.resolve(templatePath, file),
+          directory: path.resolve(directory, file),
+          variables
+        });
+      } else if (fileStats.isFile()) {
+        await renderFile({
+          templatePath: path.resolve(templatePath, file),
+          filename: path.resolve(directory, file),
+          variables
+        })
+      }
+
+    });
+
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
@@ -133,35 +176,34 @@ async function renderTemplates({template, templateFolderPath, outputDirectory, v
     if (onCreate) {
       await onCreate(variables);
     }
-
     const output = path.resolve(process.cwd(), outputDirectory);
     const dir = await fse.readdirSync(path.resolve(templateFolderPath));
-    dir.forEach( async item => {
+
+    dir.forEach( async file => {
       try {
         const EXCLUDED_FILES = ['template.config.js'];
 
-        if (!EXCLUDED_FILES.includes(item)) {
-          console.log('Rendering item: ', item);
-          console.log('PATH: ', path.resolve(templateFolderPath, item));
-          const fileStats = fse.statSync(
-            path.resolve(templateFolderPath, item)
-          );
-          // check if the file is a directory
-          if (fileStats.isDirectory()) {
-            const newDir = outputDirectory + '/item';
-            // Enter the directory and recursively build out each file
-            await renderDirectory(newDir, item, variables);
+        if (!EXCLUDED_FILES.includes(file)) {
 
-          } else if (fileStats.isFile()) {
-            console.log(item, ' is a File');
-            ejs.renderFile(
-              path.resolve(output, item),
-              variables,
-              {},
-              ( error, file ) => {
-                if (error) { throw error; }
-              }
-            );
+          // @TODO: If file === variable.someVariablenName --- Make the filename the entered variable name
+          const template = path.resolve(templateFolderPath, file);
+          const outputPath = path.resolve(output, file);
+
+          console.log(Chalk.yellow`TEMPLATE: `, template);
+          console.log(Chalk.yellow`RENDER TO: `, outputPath);
+
+          const templateStats = fse.statSync(
+            path.resolve(templateFolderPath, file)
+          );
+
+          // check if the template is a directory
+          if (templateStats.isDirectory()) {
+            const newDir = outputDirectory + `/${file}`;
+            // Enter the directory and recursively build out each file
+            await renderDirectory({ templatePath: template, directory: outputPath, variables });
+
+          } else if (templateStats.isFile()) {
+            await renderFile(outputDirectory, file, variables);
           }
         }
       } catch (error) {
@@ -169,8 +211,6 @@ async function renderTemplates({template, templateFolderPath, outputDirectory, v
       }
 
     });
-
-  // @TODO: START HERE!!
 
   if (onEnd) {
     await onEnd();
